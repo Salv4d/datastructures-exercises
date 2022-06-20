cd7c5b8cb1c43e93cae5bd5240d2f7b4ee31cfd0
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-06-19 20:14:16 -0400

    moved to recursion folder

--- collectOddValuesRecursive.js
+++
@@ -1,8 +0,0 @@
-function collectOddValues(arr) {
-  if (arr.length === 0) return [];
-  return arr[0] % 2 === 0
-    ? [...collectOddValues(arr.slice(1))]
-    : [arr[0], ...collectOddValues(arr.slice(1))];
-}
-
-collectOddValues([0, 1, 2, 3, 5, 6, 9, 8, 7, 3]);
