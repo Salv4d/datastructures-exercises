423c134b6e9464e619c4bc391491a917f36d4d1a
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-06-24 15:04:37 -0400

    bubble sort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,25 @@
+const numbers = [25, 3, 4, 98, -5, 11, 0];
+
+function bubbleSort(arr) {
+  let sortedArr = [...arr];
+  let arrChanges;
+  let n = 1;
+
+  while (arrChanges !== 0) {
+    arrChanges = 0;
+
+    for (let i = 0; i < sortedArr.length - n; i++) {
+      if (sortedArr[i] > sortedArr[i + 1]) {
+        [sortedArr[i + 1], sortedArr[i]] = [sortedArr[i], sortedArr[i + 1]];
+
+        arrChanges++;
+      }
+    }
+
+    n++;
+  }
+
+  return sortedArr;
+}
+
+console.log(bubbleSort(numbers));
